4399f5f664842978b29f1c821e755e8cd53a8fcb
Steve Crouch <s.crouch@software.ac.uk>
2026-05-07 12:15:53 +0100

    Add test suite

---
+++ tests/test_eva_analysis.py
@@ -0,0 +1,34 @@
+import pytest
+from eva_data_analysis import text_to_duration, calculate_crew_size
+
+def test_text_to_duration_float():
+    """
+    Test that text_to_duration returns expected ground truth values
+    for typical durations with a non-zero minute component
+    """
+    assert text_to_duration("10:20") == pytest.approx(10.3333333)
+
+def test_text_to_duration_integer():
+    """
+    Test that text_to_duration returns expected ground truth values
+    for typical whole hour durations
+    """
+    assert text_to_duration("10:00") == 10
+
+@pytest.mark.parametrize("input_value, expected_result", [
+    ("Valentina Tereshkova;", 1),
+    ("Judith Resnik; Sally Ride;", 2),
+])
+def test_calculate_crew_size(input_value, expected_result):
+    """
+    Test that it returns ground truth values for typical crew values
+    """
+    actual_result = calculate_crew_size(input_value)
+    assert actual_result == expected_result
+
+def test_calculate_crew_size_edge_cases():
+    """
+    Test it returns expected values for edge case where crew is an empty string
+    """
+    actual_result = calculate_crew_size("")
+    assert actual_result is None
